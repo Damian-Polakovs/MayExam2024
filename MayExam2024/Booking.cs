a96fd1eb138b0d5f90e96dc97457c7237e9ca55f
S00236491 <S00236491@LAB-D2037-19>
2024-05-13 13:05:26 +0100

    Add project files.

---
+++ MayExam2024/Booking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MayExam2024
+{
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        public DateTime BookingsDate { get; set; }
+        public int NumberOfParticipants { get; set; }
+    }
+}
---
+++ MayExam2024/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace MayExam2024
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; } 
+        public string Name { get; set; }
+        public string ContactNumber {  get; set; }
+    }
+}
